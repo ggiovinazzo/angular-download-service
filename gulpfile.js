13fb11c7077aa75edfa0a15fce21c0e2fa74ef28
Gianluca Giovinazzo <g.giovinazzo@gmail.com>
2015-06-29 12:36:44 +0200

    Automatic build and deploy with Travis CI (take-4)

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,70 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    del = require('del'),
-    pkg = require('./package.json'),
-    rename = require("gulp-rename");
+  jshint = require('gulp-jshint'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  del = require('del'),
+  pkg = require('./package.json'),
+  rename = require("gulp-rename"),
+  minimist = require('minimist'),
+  shell = require('gulp-shell');
+
+var argOptions = {
+  string: ['apikey', 'build']
+};
+
+var options = minimist(process.argv.slice(2), argOptions);
+
+var config = {
+  buildFolder: './build',
+  deploy: [
+    './build/*.js',
+    'bower.json'
+  ],
+  git: 'https://ggiovinazzo:' + options.apikey + '@' + pkg.config.deploy
+};
 
 gulp.task('build', function () {
-    return gulp.src('./src/*.js')
-        .pipe(concat(pkg.name + '.js'))
-        .pipe(gulp.dest('./build'))
-        .pipe(rename(pkg.name + '.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('./build'));
+  return gulp.src('./src/*.js')
+    .pipe(concat(pkg.name + '.js'))
+    .pipe(gulp.dest(config.buildFolder))
+    .pipe(rename(pkg.name + '.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.buildFolder));
+});
+
+gulp.task('clean-deploy', function(cb) {
+  del([pkg.config.deployFolder], cb);
+});
+
+gulp.task('clone-deploy', ['clean-deploy'], function(){
+  return gulp.src('')
+    .pipe(
+      shell(['git clone https://' + pkg.config.deploy])
+    );
+});
+
+gulp.task('copy-deploy', ['clone-deploy'], function(){
+  return gulp.src(config.deploy)
+    .pipe(
+      gulp.dest(pkg.config.deployFolder)
+    );
+});
+
+gulp.task('git-deploy', ['copy-deploy'], function(){
+  process.chdir(pkg.config.deployFolder)
+});
+
+gulp.task('deploy', ['git-deploy'], function () { 
+    return gulp.src('')
+    .pipe(
+      shell([
+        'git remote rm origin',
+        'git remote add origin ' + config.git,
+        'git tag v' + pkg.version + '-build-' + options.build + ' -a -m "Travis CI build: ' + options.build + '"',
+        'git add .',
+        'git commit -m "Automatic (Travis CI) build: ' + options.build + '"',
+        'git push origin master -f --tags'
+      ])
+    );
+    
 });
